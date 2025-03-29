ee838823a4b93edabf5fe71f9622c91ce90f6b6d
Thsering Sherpa <devshering@gmail.com>
2025-03-29 16:24:36 +0100

    initiated project

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import AudioPlayer from "@/components/AudioPlayer";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen p-6">
+      <AudioPlayer />
+    </div>
+  );
+}
---
+++ src/components/AudioPlayer.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BackgroundGradient } from "./ui/background-gradient";
+
+function AudioPlayer() {
+    return (
+        <div className="relative w-full md:max-w-5xl h-auto">
+            <BackgroundGradient className="rounded-[22px] max-w-sm p-4 sm:p-10 bg-white dark:bg-zinc-900">
+                Payer
+            </BackgroundGradient>
+        </div>
+    );
+}
+
+export default AudioPlayer;
